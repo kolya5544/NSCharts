54fac691e9256bda5626455c71387c19160db318
kolya5544 <05-kolya@bk.ru>
2021-05-31 15:31:02 +0300

    Initial release

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace NSCharts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(11, 186);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(328, 109);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 169);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Countries (doubleclick to remove)";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(347, 82);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 95);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(4, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Country to add";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 13);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Property to build the chart off";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 31);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(344, 186);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(328, 109);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(325, 82);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Confirm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(238, 31);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.Value = new System.DateTime(1990, 6, 7, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Take results from...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(444, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "To...";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(444, 31);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(238, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 19);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Custom datetime";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(692, 307);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "NS Charts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dotNS;
+using dotNS.Classes;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace NSCharts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static DotNS api = new DotNS();
+
+        public static List<Country> countries = new List<Country>();
+
+        public static Census mainCensus;
+        public static long fromTS = -1;
+        public static long toTS = -1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            api.UserAgent = "NSCharts dotNS library - nk.ax";
+            var enumList = Enum.GetNames(typeof(Census)).ToList();
+            foreach (string census in enumList)
+            {
+                comboBox1.Items.Add(census);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            try
+            {
+                var censusList = api.GetCensus(name, mainCensus, fromTS, toTS);
+                countries.Add(new Country()
+                {
+                    name = name,
+                    nodes = censusList
+                });
+                listBox1.Items.Add(name);
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Unknown error! The defined nation probably doesn't exist.");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null) return;
+            comboBox1.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = false;
+
+            if (checkBox1.Checked)
+            {
+                fromTS = ((DateTimeOffset)dateTimePicker1.Value).ToUnixTimeSeconds();
+                toTS = ((DateTimeOffset)dateTimePicker2.Value).ToUnixTimeSeconds();
+            }
+            checkBox1.Enabled = false;
+            dateTimePicker1.Enabled = false;
+            dateTimePicker2.Enabled = false;
+
+            mainCensus = (Census)Enum.Parse(typeof(Census), comboBox1.SelectedItem.ToString());
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string name = listBox1.SelectedItem.ToString();
+                countries.RemoveAll(z => z.name == name);
+                listBox1.Items.Clear();
+                countries.ForEach(z => listBox1.Items.Add(z.name));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (countries.Count < 1) { MessageBox.Show("You didn't add a single country!"); return; }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel files (*.xlsx)|*.xlsx";
+            var drs = sfd.ShowDialog();
+            if (drs != DialogResult.OK) return;
+            BuildExcel(countries, sfd.FileName);
+            MessageBox.Show($"Successfully saved to {sfd.FileName}!");
+        }
+
+        public static void BuildExcel(List<Country> countries, string filename)
+        {
+            XSSFWorkbook hssfwb = new XSSFWorkbook();
+            var sheet = hssfwb.CreateSheet("Data");
+            List<IRow> rows = new List<IRow>();
+            for (int i = 0; i < countries.Count + 1; i++)
+            {
+                rows.Add(sheet.CreateRow(i));
+            }
+            for (int i = 0; i < countries.Count; i++)
+            {
+                rows[i + 1].CreateCell(0).SetCellValue(countries[i].name);
+            }
+            rows[0].CreateCell(0).SetCellValue("Country name");
+            long lowestTimestamp = long.MaxValue;
+            long highestTimestamp = long.MinValue;
+            countries.ForEach(cn => { cn.nodes.ForEach(tn => { if (lowestTimestamp > tn.timestamp) lowestTimestamp = tn.timestamp; if (highestTimestamp < tn.timestamp) highestTimestamp = tn.timestamp; }); });
+            var dto1 = DateTimeOffset.FromUnixTimeSeconds(lowestTimestamp);
+            var dto2 = DateTimeOffset.FromUnixTimeSeconds(highestTimestamp);
+            var dto3 = dto2 - dto1;
+            var days = Math.Ceiling(dto3.TotalDays);
+            for (int i = 0; i < days; i++)
+            {
+                var newDTO = dto1 + new TimeSpan(i, 0, 0, 0);
+                var formed = newDTO.ToString("dd MMMM yyyy");
+                rows[0].CreateCell(1 + i).SetCellValue(formed);
+
+                countries.ForEach(cn =>
+                {
+                    cn.nodes.ForEach(tn =>
+                    {
+                        if (Math.Abs(tn.timestamp - newDTO.ToUnixTimeSeconds()) < 3600 * 6)
+                        {
+                            rows[countries.IndexOf(cn) + 1].CreateCell(1 + i).SetCellValue(tn.value);
+                        }
+                    });
+                });
+
+            }
+            using (FileStream stream = new FileStream(filename, FileMode.Create, FileAccess.Write))
+            {
+                hssfwb.Write(stream);
+            }
+        }
+    }
+
+    public class Country
+    {
+        public string name;
+        public List<CensusNode> nodes;
+    }
+}
